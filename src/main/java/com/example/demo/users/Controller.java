6443507239be087b4ec9fb5434a2e536edc84bd3
Amitoshverm <amitosh.verma9@gmail.com>
2025-06-03 01:47:35 +0530

    done with crud ops

--- src/main/java/com/example/demo/users/Controller.java
+++ src/main/java/com/example/demo/users/Controller.java
@@ -5,6 +5,8 @@ import com.example.demo.users.Dtos.UserDto;
 import com.example.demo.users.Dtos.UserResponseDto;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class Controller {
@@ -15,9 +17,29 @@ public class Controller {
         this.userService = userService;
     }
 
-    @PostMapping
+    @PostMapping()
     public UserResponseDto create(@RequestBody UserDto userDto) {
         return this.userService.createUser(userDto);
     }
 
+    @GetMapping()
+    public List<UserResponseDto> getAllUsers() {
+        return this.userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto getUserById(@PathVariable Long id) {
+        return this.userService.getUserById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public UserResponseDto updateUser(@PathVariable Long id, @RequestBody UserDto userdto) {
+        return this.userService.updateUser(id, userdto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        this.userService.deleteUser(id);
+        return "User deleted successfully";
+    }
 }
--- src/main/java/com/example/demo/users/Dtos/UserResponseDto.java
+++ src/main/java/com/example/demo/users/Dtos/UserResponseDto.java
@@ -6,9 +6,34 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 public class UserResponseDto {
+
     private Long id;
     private String name;
     private String email;
-} 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
